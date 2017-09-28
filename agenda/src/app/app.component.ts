31f10fa04915951bf8cf83d91490ac95be85954b
Jordi Barrero <jordibar@gmail.com>
2017-09-28 21:52:02 +0200

    Empezamos viendo los hooks

--- agenda/src/app/app.component.ts
+++ agenda/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 // Con el decorador @Component otorgamos a la clase
 // decorada comportamiento de componente
