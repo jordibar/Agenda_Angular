93d8048a593f10a39222e7615f32d83094ad48cc
Jordi Barrero <jordibar@gmail.com>
2017-09-28 12:08:02 +0200

    Aadimos comentarios a modules y components

--- agenda/src/app/app.component.ts
+++ agenda/src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
 
+// Con el decorador @Component otorgamos a la clase
+// decorada comportamiento de componente
 @Component({
+  // El metadato selector indica el selector CSS encargado
+  // de seleccionar el elemento HTML en el cual instanciar
+  // el componente
   selector: 'app-root',
+  // En templateUrl indicamos la ruta al template (html)
+  // asociado al componente
   templateUrl: './app.component.html',
+  // En styleUrls indicamos las rutas de las hojas de estilo
+  // que aplican al componente
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
